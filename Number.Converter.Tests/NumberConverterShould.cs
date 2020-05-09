cbaad29334a8f27efa69bf4949f02321fe964bf0
Sify K Raphy <sifykarippery@gmail.com>
2020-05-09 12:50:37 +0530

    added test for thousands and millions

--- Number.Converter.Tests/NumberConverterShould.cs
+++ Number.Converter.Tests/NumberConverterShould.cs
@@ -17,5 +17,19 @@ namespace Number.Converter.Tests
             
         }
 
+
+        [Fact]
+        public void GenerateCorrectSpeelingForHundredsorThrousands()
+        {
+            Assert.Equal("One Hundred", ((int)100).ConvertToSpelling());
+            Assert.Equal("One Thousand", ((int)1000).ConvertToSpelling());
+            Assert.Equal("Ten Thousand", ((int)10000).ConvertToSpelling());
+            Assert.Equal("One Hundred Thousand", ((int)100000).ConvertToSpelling());
+            Assert.Equal("One Million", ((int)1000000).ConvertToSpelling());
+            Assert.Equal("Seven Million", ((int)7000000).ConvertToSpelling());
+            Assert.Equal("Seven Million Two Hundred Fifty", ((int)7000250).ConvertToSpelling());
+
+        }
+
     }
 }
